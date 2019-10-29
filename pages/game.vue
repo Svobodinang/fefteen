89ce10d826e97a499971ea91eb824c6a4691861f
nadya <svobodinang@gmail.com>
2019-10-29 16:00:30 +0300

    улучшения кода

--- pages/game.vue
+++ pages/game.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.14.1/lodash.min.js"></script>
     <a class="back" @click.prevent="back">&#10148;</a>
     <menu>
       <div class="info">
@@ -9,49 +8,18 @@
       </div>
       <div class="info">
         <h2>Время</h2>
-        <p>{{ hours }}:{{ minutes }}:{{ secunds }}</p>
+        <p>{{ hours }}:{{ minutes }}:{{ seconds }}</p>
       </div>
     </menu>
 
     <transition-group name="fifteen-block" class="fifteen-block">
       <button
-        v-for="(button, indexButton) in buttons[0]"
+        v-for="(button, indexButton) in buttons"
         v-bind:key="button"
-        v-on:click="change(button, 0, indexButton)"
-        :class="{null: button==0}"
+        v-on:click="onClick(button, indexButton)"
+        :class="{null: button === 0}"
       >
         <span v-if="button != 0">{{ button }}</span>
-        <span v-else></span>
-      </button>
-
-      <button
-        v-for="(button, indexButton) in buttons[1]"
-        v-bind:key="button"
-        v-on:click="change(button, 1, indexButton)"
-        :class="{null: button==0}"
-      >
-        <span v-if="button != 0">{{ button }}</span>
-        <span v-else></span>
-      </button>
-
-      <button
-        v-for="(button, indexButton) in buttons[2]"
-        v-bind:key="button"
-        v-on:click="change(button, 2, indexButton)"
-        :class="{null: button==0}"
-      >
-        <span v-if="button != 0">{{ button }}</span>
-        <span v-else></span>
-      </button>
-
-      <button
-        v-for="(button, indexButton) in buttons[3]"
-        v-bind:key="button"
-        v-on:click="change(button, 3, indexButton)"
-        :class="{null: button==0}"
-      >
-        <span v-if="button != 0">{{ button }}</span>
-        <span v-else></span>
       </button>
     </transition-group>
   </div>
@@ -60,99 +28,76 @@
 <script>
 export default {
   data: () => ({
-    secunds: 0,
+    // время
+    seconds: 0,
     minutes: 0,
     hours: 0,
-    buttons: [
-      [1, 2, 3, 4].sort(function() {
-        return Math.random() - 0.5;
-      }),
-      [5, 6, 7, 8].sort(function() {
-        return Math.random() - 0.5;
-      }),
-      [9, 10, 11, 12].sort(function() {
-        return Math.random() - 0.5;
-      }),
-      [13, 14, 15, 0].sort(function() {
-        return Math.random() - 0.5;
-      })
-    ].sort(function() {
+    buttons: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0].sort(function() {
       return Math.random() - 0.5;
     }),
+    // счет
     number: 0,
+    // границы поля
+    lBorder: [0, 4, 8, 12],
+    rBorder: [3, 7, 11, 15],
+    vS: 4,
+    hS: 1   
   }),
   mounted () {
     this.interval = setInterval(() => {
-      if (this.secunds < 60) this.secunds++
+      if (this.seconds < 60) this.seconds++
       else if (this.minutes < 60) { 
         this.minutes++
-        this.secunds = 0
+        this.seconds = 0
       }
       else {
         this.hours++
         this.minutes = 0
-        this.secunds = 0
+        this.seconds = 0
       }
     }, 1000);
   },
   methods: {
-    change: function(button, indexLine, indexButton) {
-      let newButtons = []
-      for (let i = 0; i < this.buttons.length; i++) {
-        newButtons.push([])
-        for (let j = 0; j < this.buttons[i].length; j++) {
-          newButtons[i].push(this.buttons[i][j])
-        }
-      }
+    onClick: function(button, indexButton) {
+      let newButtons = this.createNewButtons()
+      let indexesOppo = [] // возможные варианты расположения нуля
+      
+      if (indexButton > this.rBorder[0]) indexesOppo.push(indexButton - this.vS)
+      if (indexButton < this.lBorder[3]) indexesOppo.push(indexButton + this.vS)
+      if (indexButton > this.lBorder[0] && !this.inter(indexButton, this.lBorder)) indexesOppo.push(indexButton - this.hS)
+      if (indexButton < this.rBorder[3] && !this.inter(indexButton, this.rBorder)) indexesOppo.push(indexButton + this.hS)
 
-      if (indexLine != 0) {
-        if (newButtons[indexLine - 1][indexButton] === 0) {
-          newButtons[indexLine - 1][indexButton] = button
-          newButtons[indexLine][indexButton] = 0
-          this.number += 1
+      for (let index of indexesOppo) {
+        if (newButtons[index] === 0) {
+          newButtons[index] = button
+          newButtons[indexButton] = 0
         }
-      }
-
-      if (indexButton != 0) {
-        if (newButtons[indexLine][indexButton - 1] === 0) {
-          newButtons[indexLine][indexButton - 1] = button
-          newButtons[indexLine][indexButton] = 0
-          this.number += 1
-        }
-      }
-
-      if (indexButton != newButtons[indexLine].length - 1) {
-        if (newButtons[indexLine][indexButton + 1] === 0) {
-          newButtons[indexLine][indexButton + 1] = button
-          newButtons[indexLine][indexButton] = 0
-          this.number += 1
-        }
-      }
-
-      if (indexLine != newButtons.length - 1) {
-        if (newButtons[indexLine + 1][indexButton] === 0) {
-          newButtons[indexLine + 1][indexButton] = button
-          newButtons[indexLine][indexButton] = 0
-          this.number += 1
-        }
-      }
-
+      }     
+      
       this.buttons = newButtons
-
-      let endGame = true
-      for (let i=0; i<this.buttons.length; i++) {
-        for (let j=0; j<this.buttons[i].length; j++) {
-          if (this.buttons[i][j] != 0 && this.buttons[i][j] != (i*4)+(j+1)) {
-            endGame = false
-            break
-          }
-        }
-      }
-      if (endGame) alert("Вы выиграли")
-
+      if (this.checkEndGame()) alert("Вы выиграли")
     },
     back: function() {
       this.$router.push('/')
+    },
+    
+    createNewButtons: function() {
+      let newButtons = []
+      for (let i = 0; i < this.buttons.length; i++) {
+        newButtons.push(this.buttons[i])
+      }
+      return newButtons      
+    },
+    checkEndGame: function() {
+      let endGame = true
+      for (let i=0; i<this.buttons.length - 1; i++) {
+        if (this.buttons[i] != i+1) endGame = false;
+      }
+      return endGame
+    },
+    inter: function(el, arr1) {
+      if (arr1.indexOf(el) != -1) return true
+      return false
     }
   },
   beforeDestroy () {
@@ -162,13 +107,13 @@ export default {
 </script>
 
 <style scoped>
-.back{
+.back {
   font-size: 3rem;
-  color:#3F3B70;
+  color: #3f3b70;
   margin-left: 100px;
   margin-top: 20px;
   writing-mode: tb-rl;
-  transform:rotate(90deg);
+  transform: rotate(90deg);
 }
 .back:hover {
   cursor: pointer;
@@ -185,7 +130,7 @@ menu {
   margin-top: -40px;
 }
 .info {
-  background: #D4D0FB;
+  background: #d4d0fb;
   color: #262343;
 }
 .info h2,
